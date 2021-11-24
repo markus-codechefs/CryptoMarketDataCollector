62da4110ccb35bd08c27aaafe460a153c865b469
Markus Trachsel <markus@codechefs.ch>
2021-11-24 10:51:56 +0100

    Added a test project

---
+++ Tests/SchedulerTriggerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using System;
+
+namespace Tests;
+
+public class SchedulerTriggerTests
+{
+    [Fact]
+    public void SimplePassingTest()
+    {
+        Assert.Equal(4, 4); 
+    }
+}
